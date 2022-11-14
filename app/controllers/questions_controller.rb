107fc170c2c41d42de8113ac9708a3c61d347886
Andris Asper <andris.asper@gmail.com>
2022-11-14 14:26:47 +0100

    3rd commit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
-
     if params[:question]
-      if @question === "I am going to work"
-        @answer = "Great!"
-      elsif @question.include?("?")
-        @answer = "Silly question, get dressed and go to work!"
-      else
-        @answer = "I don't care, get dressed and go to work!"
-      end
+      @answer = if @question == 'I am going to work'
+                  'Great!'
+                elsif @question.match(/^\w+[?]$/)
+                  'Silly question, get dressed and go to work!'
+                else
+                  "I don't care, get dressed and go to work!"
+                end
     end
   end
 end
